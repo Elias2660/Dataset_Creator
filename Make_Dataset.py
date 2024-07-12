d16e6e066c0f30710dfcdb9db85802827907f706
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-11 22:19:10 -0400

    add more work

--- Make_Dataset.py
+++ Make_Dataset.py
@@ -23,8 +23,6 @@ the format of the new processed dataframe would be
 1. time, name, class, start frame, end frame, 
 """
 
-def get_class(frame_name: str):
-    ...
 
 
 # Add time data to the counts.csv
@@ -33,8 +31,8 @@ def process_frame_count(counts: pd.DataFrame):
     processed_counts = pd.DataFrame()
     processed_counts["time"] = pd.to_datetime(counts["Filename"].str.replace(".h264", "").replace(".mp4", ""), format="%Y-%m-%d %H:%M:%S.%f")
     processed_counts["filename"] = counts["Filename"]
-    processed_counts["class"] = "NaN"
-    processed_counts["start frame"] = "NaN"
+    processed_counts["class"] = np.nan
+    processed_counts["start frame"] = np.nan
     processed_counts["end frame"] = counts["Frame count"]
     return processed_counts
     
@@ -42,9 +40,42 @@ processed_counts = process_frame_count(counts)
 
 # %%
 
-def process_log_files(log:pd.DataFrame):
+def process_log_files(log:pd.DataFrame, classNum:int):
     processed_log = pd.DataFrame()
-    processed_log["time"] = pd.to_datetime(log["year/month/day_hour/min/sec"], format="%Y%m%d_%H%M%S")
-    processed_log["filename"] = "NaN"
-    processed_log["class"] = 
+    processed_log["time"] = pd.to_datetime(log["frame_name"], format="%Y%m%d_%H%M%S")
+    processed_log["filename"] = np.nan
+    processed_log["class"] = classNum
+    processed_log["start frame"] = np.nan
+    processed_log["end frame"] = np.nan
+    return processed_log
+
+processed_logNeg = process_log_files(logNeg, 0)
+processed_logNo = process_log_files(logNo, 1)
+processed_logPos = process_log_files(logPos, 2)
+
 # %%
+
+FPS = 25
+
+def create_dataset(processed_counts: pd.DataFrame, *args):
+    dataset = pd.concat([processed_counts, *args], ignore_index=True)
+    dataset = dataset.sort_values(by="time").reset_index(drop=True)
+    # for filenames
+    
+    dataset["filename"] = dataset["filename"].ffill()
+    dataset = dataset.dropna(subset=["filename"]).reset_index(drop=True)
+    
+    # for classes
+    
+    
+    # for start frames
+    
+    
+    # for end frames
+    
+    
+    return dataset
+dset = create_dataset(processed_counts, processed_logNeg, processed_logNo, processed_logPos)
+print(dset)
+# %%
+
