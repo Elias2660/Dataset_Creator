51f91639320dc1a39b05fa68a5a557c705c0cd8c
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-07-11 21:36:07 -0400

    add some stuff

--- Make_Dataset.py
+++ Make_Dataset.py
@@ -13,9 +13,9 @@ import argparse
 
 # parser.add_argument('--input', type=str, help='Input file path')
 counts = pd.read_csv('counts.csv')
-logNo = pd.read_csv('logNo.txt', names=["year/month/day_hour/min/sec"])
-logPos = pd.read_csv('logPos.txt', names=["year/month/day_hour/min/sec"])
-logNeg = pd.read_csv('logNeg.txt', names=["year/month/day_hour/min/sec"])
+logNo = pd.read_csv('logNo.txt', names=["frame_name"])
+logPos = pd.read_csv('logPos.txt', names=["frame_name"])
+logNeg = pd.read_csv('logNeg.txt', names=["frame_name"])
 # %%
 
 """
@@ -23,12 +23,16 @@ the format of the new processed dataframe would be
 1. time, name, class, start frame, end frame, 
 """
 
+def get_class(frame_name: str):
+    ...
+
+
 # Add time data to the counts.csv
 
 def process_frame_count(counts: pd.DataFrame):
     processed_counts = pd.DataFrame()
     processed_counts["time"] = pd.to_datetime(counts["Filename"].str.replace(".h264", "").replace(".mp4", ""), format="%Y-%m-%d %H:%M:%S.%f")
-    processed_counts["Filename"] = counts["Filename"]
+    processed_counts["filename"] = counts["Filename"]
     processed_counts["class"] = "NaN"
     processed_counts["start frame"] = "NaN"
     processed_counts["end frame"] = counts["Frame count"]
@@ -40,4 +44,7 @@ processed_counts = process_frame_count(counts)
 
 def process_log_files(log:pd.DataFrame):
     processed_log = pd.DataFrame()
-    
+    processed_log["time"] = pd.to_datetime(log["year/month/day_hour/min/sec"], format="%Y%m%d_%H%M%S")
+    processed_log["filename"] = "NaN"
+    processed_log["class"] = 
+# %%
