37bbfbce093f4f50bbc63b2d64e38d625cb25649
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-11-10 21:39:37 -0500

    updates

--- one_class_runner.py
+++ one_class_runner.py
@@ -16,21 +16,23 @@ if __name__ == "__main__":
         description="Special case for dataprep videos where each video is one different class"
     )
     parser.add_argument(
-        "--path", type=str, help="path to where directory is located", default="."
+        "--path", type=str, help="path to where directory is located", default=".", required=False
     )
     parser.add_argument(
-        "--counts", type=str, help="path to counts file, default is counts.csv", default="counts.csv"
+        "--counts", type=str, help="path to counts file, default is counts.csv", default="counts.csv", required=False
     )
     parser.add_argument(
-        "--start-frame", type=int, help="start frame, default zero", default=0
+        "--start-frame", type=int, help="start frame, default zero", default=0, required=False
         )
     parser.add_argument(
-        "--end-frame-buffer", type=int, help="the end frame that one would use, default 0", default=0
+        "--end-frame-buffer", type=int, help="the end frame that one would use, default 0", default=0, required=False
         )
     parser.add_argument(
-        "--splits", type=int, help="number of splits per video, default 4", default=4
+        "--splits", type=int, help="number of splits per video, default 4", default=4, required=False
     )
     args = parser.parse_args()
+    logging.info(f"Arguments: path={args.path}, counts={args.counts}, start_frame={args.start_frame}, end_frame_buffer={args.end_frame_buffer}, splits={args.splits}")
+    
     counts = pd.read_csv(os.path.join(args.path, args.counts))
     
     final_dataframe = pd.DataFrame(columns=["filename", "class", "beginframe", "endframe"])
